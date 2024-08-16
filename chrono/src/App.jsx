32af7dbf7a698def56c8d14dad48e0372f93af48
hamdi <hamdiabdallah681@gmail.com>
2024-08-16 07:12:30 +0100

    stopwatch with react

---
+++ chrono/src/App.jsx
@@ -0,0 +1,11 @@
+import Stopwatch from "../components/StopWatch/Stopwatch";
+
+function App() {
+  return (
+    <div className="App">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
